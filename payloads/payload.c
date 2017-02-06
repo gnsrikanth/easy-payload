29cb82b81fa2ae36b50f8400f6c62005203ff416
gnsrikanth <nagasrikanthgrandhi@gmail.com>
2017-02-06 05:44:26 +0000

    Create payload.c

---
+++ payloads/payload.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+void main ()
+{
+	char command[50];
+	int op1,a=1;
+{
+//edit here for paylods
+printf("Enter 1 to generate windows payload\n    Enter 2 to generate android payload");
+
+scanf("%d",&op1);
+
+switch ( op1 ) {
+case 1: //Windows payload
+	strcpy(command, "bash windows_reverse_tcp.sh");
+	system(command);
+	break;
+case 2: //Android payload
+	strcpy(command, "bash android_reverse_tcp.sh");
+	system(command);
+	break;
+
+default:
+	printf("enter 1 - 2 only")
+  	break;
+}
+	
+
+
+} 
