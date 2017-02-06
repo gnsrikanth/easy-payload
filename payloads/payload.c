4573a3a98ad0f73db9206317d00c1daabfb0ee4c
gnsrikanth <nagasrikanthgrandhi@gmail.com>
2017-02-06 06:33:27 +0000

    Update payload.c

--- payloads/payload.c
+++ payloads/payload.c
@@ -4,28 +4,29 @@
 void main ()
 {
 	char command[50];
-	int op1,a=1;
+	int op1;
 {
-//edit here for paylods
-printf("Enter 1 to generate windows payload\n    Enter 2 to generate android payload");
+//edit here for PAYLOADS************
+printf("Enter 1 to generate windows payload\nEnter 2 to generate android payload");
+printf("\nEnter Option:");
 
 scanf("%d",&op1);
 
 switch ( op1 ) {
-case 1: //Windows payload
+case 1: //Windows payload***********
 	strcpy(command, "bash windows_reverse_tcp.sh");
 	system(command);
 	break;
-case 2: //Android payload
+case 2: //Android payload***********
 	strcpy(command, "bash android_reverse_tcp.sh");
 	system(command);
 	break;
 
 default:
-	printf("enter 1 - 2 only")
+	printf("enter 1 - 2 only");
   	break;
 }
 	
-
+}
 
 } 
