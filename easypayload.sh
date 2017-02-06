215e8dae325da84da7acc987ed02e3c5a65fc1cb
gnsrikanth <nagasrikanthgrandhi@gmail.com>
2017-02-06 06:30:19 +0000

    Update easypayload.sh

--- easypayload.sh
+++ easypayload.sh
@@ -4,6 +4,8 @@ echo "###########by Srikanth################"
 echo "######################################"
 echo "Enter 1 to generate payload"
 echo "Enter 2 to Exploit(Msfconsole)"
+echo "Enter 3 to exit"
+echo "Enter option:"
 read op1
 		
 case $op1 in
@@ -15,6 +17,7 @@ case $op1 in
 	2) cd exploit
 	cc exploit.c
 	./a.out;;
+	3)^C;;
      #for worng input
 	*) echo "INVALID NUMBER!" ;;
 esac
